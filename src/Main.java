8ae01dfafc96686113376d442e0cef9c7b1681ed
Sanzharbek Zhalilov <arapovich.s@gmail.com>
2022-10-06 09:55:23 +0300

    Fixed bug

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(isPalindrome(5775));
+        System.out.println(isPalindrome(-121));
     }
 
     public static boolean isPalindrome(int x) {
         int reverse = 0;
         int temp = x;
 
-        while (temp != 0) {
+        while (temp > 0) {
             reverse = (reverse * 10) + (temp % 10);
             temp /= 10;
         }
